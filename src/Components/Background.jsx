332462c2d3de9e2a4e0752a32068ce694106f0eb
VishalSingh3026 <vishalsingh3026@gmail.com>
2024-06-25 18:20:19 +0530

    first

---
+++ src/Components/Background.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Background = () => {
+  return (
+    <>
+     <div className='w-full py-10 absolute top-[5%] text-zinc-500 text-xl flex justify-center font-semibold '>Document</div>
+      <h1 className='text-[16vw] text-zinc-900 absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] leading-none tracking-tighter'>
+        Docs
+      </h1>
+    </>
+  )
+}
+
+export default Background
